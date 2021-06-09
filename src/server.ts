f6818385ae50d8fd22f394325818cdd5bd026874
FelipeBigarelli <felipebigarelli@hotmail.com>
2021-06-09 11:34:56 -0300

    .

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,13 @@ import './database/connection';
 
 import routes from './routes';
 
+const port = 3333;
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.listen(3333);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
+});
